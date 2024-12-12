ad9c454be38b062639d4251be58dfccb84ce78c1
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2024-12-12 17:52:04 +1300

    WIP day 6 part 2

--- src/d06.clj
+++ src/d06.clj
@@ -26,6 +26,57 @@
     (= (get-in grid [y x]) \#)
     false))
 
+(defn next-obstacle
+  "Find the position of the next obstacle in the given direction"
+  [grid x y dir]
+  (case dir
+    :north (let [found (->> (take y grid)
+                            (map #(nth % x))
+                            (keep-indexed #(when (= %2 \#) %1))
+                            last)]
+             (when found
+               [x found]))
+    :south (let [y-start (+ y 1)
+                 found (->> (drop y-start grid)
+                            (map #(nth % x))
+                            (keep-indexed #(when (= %2 \#) %1))
+                            (#(do (println %) %))
+                            first)]
+             (when found
+               [x (+ y-start found)]))
+    :west (let [found (->> (nth grid y)
+                           (take x)
+                           (keep-indexed #(when (= %2 \#) %1))
+                           last)]
+            (when found
+              [found y]))
+    :east (let [x-start (+ x 1)
+                found (->> (nth grid y)
+                           (drop x-start)
+                           (keep-indexed #(when (= %2 \#) %1))
+                           first)]
+            (when found
+              [(+ x-start found) y]))))
+
+(defn before-next-obstacle
+  "Find the position just before the next obstacle in the given direction"
+  [grid x y dir]
+  (let [obs (next-obstacle grid x y dir)]
+    (when obs
+      (let [[ox oy] obs]
+        (case dir
+          :north [ox (inc oy)]
+          :south [ox (dec oy)]
+          :west [(inc ox) oy]
+          :east [(dec ox) oy])))))
+
+(defn dir-op [dir]
+  (case dir
+    :north :south
+    :south :north
+    :west :east
+    :east :west))
+
 (defn d06 [file]
   (let [grid (->> (slurp file)
                   (str/split-lines))
@@ -47,6 +98,26 @@
                            (as-> v v
                              (assoc v 0 (next 0))
                              (assoc v 1 (next 1)))))))))
+      (println (count @pts)))
+    (let [pos (atom (conj start :north))
+          blocks (atom #{})
+          pts (atom #{})]
+      (while (in-bounds grid @pos)
+        (let [currdir (nth @pos 2)
+              next (conj (next-step @pos currdir) currdir)]
+          (if (is-obstacle grid next)
+            (swap! pos #(conj (vec (take 2 %)) (rot (nth % 2))))
+            (do
+              (let [check-passed (before-next-obstacle grid (first @pos) (second @pos) (rot (nth @pos 2)))]
+                (println @pos next)
+                (when (and
+                       check-passed
+                       (some (vector (first @pos) (second @pos) (dir-op (nth @pos 2))) @pts)
+                       (not (contains? @pts @pos)))
+                  (println next)
+                  (swap! blocks conj ((first next) (second next))))
+                (swap! pts conj @pos)
+                (swap! pos (fn [_] (vec next))))))))
       (println (count @pts)))))
 
 ; part 2 pseudo description because tired - implement tomorrow
